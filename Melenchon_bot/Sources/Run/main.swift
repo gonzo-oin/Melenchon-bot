48baefc1a4aad2f1b91f2fd800794869338dd97a
Gonzo Oin <gerardotstephane@gmail.com>
2017-06-04 16:50:28 +0200

    chain calls for getting caption

--- Melenchon_bot/Sources/Run/main.swift
+++ Melenchon_bot/Sources/Run/main.swift
@@ -17,37 +17,70 @@ import HTTP
 /// .run() runs the Droplet's commands, 
 /// if no command is given, it will default to "serve"
 
-
-let config = try Config()
-try config.setup()
-
+// Declaring vars
 let Client_id = "<client_id>"
 let Client_secret = "<client_secret>"
 let Refresh_token = "<refresh_token>"
 let API_Key = "<API_key>"
 let Tmft = "srt"
 var Access_token = "<Access Token>"
+
+// Instantiate a Drop
+let config = try Config()
+try config.setup()
 let drop = try Droplet(config)
 
-let Token_refresh = try Droplet(config)
 
-let test = try Droplet(config)
+// Declaring Methods
+func refreshToken() -> String? {
+	do {
+		let response = try drop.client.post("https://www.googleapis.com/oauth2/v4/token",
+		                                    query: [
+												"client_id": Client_id,
+												"grant_type": "refresh_token",
+												"client_secret": Client_secret,
+												"refresh_token": Refresh_token],
+		                                    ["Content-Type":" application/x-www-form-urlencoded"])
+		
+		if let bodyBytes = response.body.bytes, let json = try? JSON(bytes: bodyBytes) {
+			print("new access token:" + json["access_token"]!.string!)
+			return json["access_token"]!.string!
+		}
+	} catch {
+		// will print error catched in try calls
+		print(error)
+	}
 
-
-
-drop.get("/Token_refresh") { _ in
-
-    return try Token_refresh.client.post("https://www.googleapis.com/oauth2/v4/token", query: ["client_id":Client_id,"grant_type":"refresh_token","client_secret":Client_secret, "refresh_token":Refresh_token],["Content-Type":"application/x-www-form-urlencoded"])
-    
+	return nil
 }
 
-drop.get("/hello") { _ in
-    return "Hello Vapor"
+func getCaption(id: String) -> String? {
+	do {
+		let response = try drop.client.get("https://www.googleapis.com/youtube/v3/captions/" + id,
+		                                   query: [
+											"key": API_Key,
+											"tmft":Tmft],
+		                                   ["authorization":"Bearer " + Access_token])
+		
+		if let bodyBytes = response.body.bytes, let captionString = String(bytes: bodyBytes, encoding: .utf8) {
+			print("captionString:\n" + captionString)
+			return captionString
+		}
+		
+	} catch {
+		// will print error catched in try calls
+		print(error)
+	}
+	return nil
 }
 
-drop.get("/test") { _ in
-    
-    return try test.client.get("https://www.googleapis.com/youtube/v3/captions/nR2uYc0Woj92voYRsp_FapLspS9Bmvxq", query: ["key": API_Key,"tmft":Tmft], ["authorization":"Bearer "+Access_token])
-   }
 
+// Call methods
+if let newToken = refreshToken() {
+	Access_token = newToken
+
+	let captionString = getCaption(id: "nR2uYc0Woj92voYRsp_FapLspS9Bmvxq")
+}
+
+// Start HTTP Server with no routes
 try drop.run()
